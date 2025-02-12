87962edb7db7513c0d9ede4c44940317be09f4db
Kartikjarali010 <kartikjarali@gmail.com>
2025-02-12 17:58:59 +0530

    created 2 components and updated home_page

--- frontend/lib/pages/common_pages/home_page.dart
+++ frontend/lib/pages/common_pages/home_page.dart
@@ -1,6 +1,8 @@
-import 'package:dev_track_app/components/custom_button.dart';
+
+import 'package:dev_track_app/utils/bottom_nav_bar.dart';
+import 'package:dev_track_app/utils/custom_button.dart';
 import 'package:flutter/material.dart';
-import 'package:dev_track_app/components/bottom_nav_bar.dart';
+
 import 'package:dev_track_app/pages/common_pages/login_page.dart';
 import 'package:dev_track_app/pages/common_pages/register_page.dart';
 
