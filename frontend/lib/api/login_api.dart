ce4dd9e057b6c250713fde3435d92d1cc850b11e
yvaishalirao <yellurivaishalirao@gmail.com>
2025-03-09 21:18:48 +0530

    api and viewmodel for logout

--- frontend/lib/api/login_api.dart
+++ frontend/lib/api/login_api.dart
@@ -8,14 +8,14 @@ import '../models/login_model.dart';
 // static const String loginEndpoint = "$baseUrl/user/login";
 
 class AuthService {
-  final String baseUrl = "https://dev-track-app.onrender.com/api/user/login";
+  final String baseUrl = "https://dev-track-app.onrender.com/api/user";
 
   Future<LoginModel> login(String email, String password) async {
     try {
       print("Logging in ... ");
 
       final response = await http.post(
-        Uri.parse(baseUrl),
+        Uri.parse("$baseUrl/login"),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({"email": email, "password": password}),
       );
@@ -40,6 +40,40 @@ class AuthService {
       }
     } catch (e) {
       throw Exception('Error: $e');
+    }
+  }
+
+  Future<void> logout() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      String? sessionCookie = prefs.getString('session_cookie');
+      String? csrfToken = prefs.getString('csrf_token');
+
+      if (sessionCookie != null && csrfToken != null) {
+        final response = await http.post(
+          Uri.parse("$baseUrl/logout"),
+          headers: {
+            'Content-Type': 'application/json',
+            'Cookie': sessionCookie,
+            'X-CSRFToken': csrfToken,
+          },
+        );
+
+        if (response.statusCode == 200) {
+          print("Logout successful");
+        } else {
+          print("Failed to logout: ${response.body}");
+        }
+      }
+
+      // Clear stored session details after logout
+      await prefs.remove('role');
+      await prefs.remove('csrf_token');
+      await prefs.remove('session_cookie');
+
+      print("User session cleared from SharedPreferences");
+    } catch (e) {
+      throw Exception('Error during logout: $e');
     }
   }
 }
